a193da24befa54de7d8e630f78234c5716a33578
damirsamir <156499587+damirsamir@users.noreply.github.com>
2024-01-13 11:40:48 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+'use strict';
+
+document.addEventListener('DOMContentLoaded', function(e) {
+    const btn = document.querySelector('.header_hamburger'),
+          menu = document.querySelector('.header_menu');
+
+    btn.addEventListener('click', e => {
+        menu.classList.toggle('header_menu_active');
+        btn.classList.toggle('header_hamburger_close');
+    });
+});
